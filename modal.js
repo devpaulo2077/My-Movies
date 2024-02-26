7d52ac3071cfae36a3c25de8726621ff3c45e563
Paulo Ricardo <dev.paulorc@gmail.com>
2024-02-26 12:17:16 -0300

    🆙 UPDATE

--- modal.js
+++ modal.js
@@ -10,48 +10,26 @@ function createModal(dados){
     modalContainer.innerHTML = `
     <h2 id="movie-title">${dados.Title} - ${dados.Year}</h2>
     <section id="modal-body">
-        <img id="movie-poster" src="${dados.Poster}" alt="Poster do Filme">
-        <div id="movie-info">
-            <h3 id="movie-plot">${dados.Plot}</h3>
-            <div id="movie-cast">
-                <h4>Elenco</h4>
-                <h5>${dados.Actors}</h5>
-            </div>
-            <div id="movie-genre">
-                <h4>Gênero</h4>
-                <h5>${dados.Genre}</h5>
-            </div>
-        </div>
-
-        <section id="modal-footer">
-            <button id="add-to-list">Adicionar à lista</button>
-        </section>
-    </section>`
-}
-
-// modalContainer.innerHTML = `
-// <h2 id="movie-title">${dados.Title} - ${dados.Year}</h2>
-// <section id="modal-body">
-// <img
-// id="movie-poster"
-// src=${dados.Poster}
-// alt="Poster do Filme."
-// />
-// <div id="movie-info">
-// <h3 id="movie-plot">
-// ${dados.Plot}
-// </h3>
-// <div id="movie-cast">
-// <h4>Elenco:</h4>
-// <h5>${dados.Actors}</h5>
-// </div>
-// <div id="movie-genre">
-// <h4>Gênero:</h4>
-// <h5>${dados.Genre}</h5>
-// </div>
-// </div>
-// </section>
-// <section id="modal-footer">
-// <button id="add-to-list">Adicionar à Lista</button>
-// </section>`;
-// }
+    <img
+    id="movie-poster"
+    src=${dados.Poster}
+    alt="Poster do Filme."
+    />
+    <div id="movie-info">
+    <h3 id="movie-plot">
+    ${dados.Plot}
+    </h3>
+    <div id="movie-cast">
+    <h4>Elenco:</h4>
+    <h5>${dados.Actors}</h5>
+    </div>
+    <div id="movie-genre">
+    <h4>Gênero:</h4>
+    <h5>${dados.Genre}</h5>
+    </div>
+    </div>
+    </section>
+    <section id="modal-footer">
+    <button id="add-to-list">Adicionar à Lista</button>
+    </section>`;
+    }
