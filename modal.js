bd7415fcff12f5e05877238710279b25b51c7ba8
Paulo Ricardo <dev.paulorc@gmail.com>
2024-02-27 10:12:13 -0300

    🆙 UPDATE

--- modal.js
+++ modal.js
@@ -1,35 +1,51 @@
 const background = document.getElementById('modal-background')
 const modalContainer = document.getElementById('modal-container')
 
+let currentMovie = {};
+
 background.addEventListener('click', function(){
     overlay.classList.remove('open')
     overlay.classList.add('overlay')
 })
 
+function closeModal() {
+    overlay.classList.remove('open');
+    overlay.classList.add('overlay');
+}
+
 function createModal(dados){
+    currentMovie = dados;
+    
     modalContainer.innerHTML = `
     <h2 id="movie-title">${dados.Title} - ${dados.Year}</h2>
     <section id="modal-body">
-    <img
-    id="movie-poster"
-    src=${dados.Poster}
-    alt="Poster do Filme."
-    />
-    <div id="movie-info">
-    <h3 id="movie-plot">
-    ${dados.Plot}
-    </h3>
-    <div id="movie-cast">
-    <h4>Elenco:</h4>
-    <h5>${dados.Actors}</h5>
-    </div>
-    <div id="movie-genre">
-    <h4>Gênero:</h4>
-    <h5>${dados.Genre}</h5>
-    </div>
-    </div>
+        <img id="movie-poster" src=${dados.Poster} alt="Poster do Filme." />
+        <div id="movie-info">
+            <h3 id="movie-plot">${dados.Plot}</h3>
+            <div id="movie-cast">
+                <h4>Elenco:</h4>
+                <h5>${dados.Actors}</h5>
+            </div>
+            <div id="movie-genre">
+                <h4>Gênero:</h4>
+                <h5>${dados.Genre}</h5>
+            </div>
+        </div>
     </section>
     <section id="modal-footer">
-    <button id="add-to-list">Adicionar à Lista</button>
+        <button id="add-to-list" onclick='addCurrentMovieToList()'>Adicionar à Lista</button>
     </section>`;
+}
+
+function addCurrentMovieToList() {
+    if(isMovieAlreadyOnlist(currentMovie.imdbID)){
+        return notie.alert({
+            type: 'error',
+            text: 'O filme já está na sua lista'
+        }) 
     }
+    addToList(currentMovie);
+    updateUI(currentMovie);
+    updateLocalStorage();
+    closeModal();
+}
--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const $searchButton = document.getElementById('search-btn');
 const overlay = document.getElementById('modal-overlay');
 const inputName = document.getElementById('movie-name');
 const InputYear = document.getElementById('movie-year');
+const movieListContainer = document.getElementById('movie-list');
+
+// let movieList = [];
+let movieList = JSON.parse(localStorage.getItem('movieList')) ?? [];
 
 $searchButton.addEventListener('click', async function(){
     
@@ -33,7 +37,7 @@ $searchButton.addEventListener('click', async function(){
 
 
 function geradormMovieName(){
-    if (inputName === ''){
+    if (inputName.value === ''){
         throw new Error('O nome do filme deve ser informado')
     } else {
         return inputName.value.split(' ').join('+')
@@ -48,4 +52,44 @@ function geradormMovieYear(){
         return notie.alert({type: 'error' ,text: "Ano do filme inválido"});
     }
     return `&y=${InputYear.value}`
+}
+
+function addToList(movieObject){
+    movieList.push(movieObject)
+}
+
+
+function updateUI(movieObject){
+    movieListContainer.innerHTML += `
+    <article id="movie-card${movieObject.imdbID}">
+        <img src="${movieObject.Poster}" alt="Poster de ${movieObject.Title}">
+        <button class="remove-btn" onclick="removeFilmFromList('${movieObject.imdbID}')"><i class="bi bi-trash"></i>Remover</button>
+    </article>`
+}
+
+function removeFilmFromList(id){
+    notie.confirm({
+        text: "Deseja remover o filme de sua lista?",
+        submitText: 'Sim',
+        cancelText: 'Não',
+        position: 'top',
+        submitCallback: function remove(){
+            movieList = movieList.filter((movie) => movie.imdbID !== id);
+            document.getElementById(`movie-card${id}`).remove();
+            updateLocalStorage();
+        },
+    })
+}
+
+function isMovieAlreadyOnlist(id){
+    const doesThisIdBelongToThisMovie = (movieObject) => movieObject.imdbID === id;
+    return !!movieList.find(doesThisIdBelongToThisMovie);
+}
+
+function updateLocalStorage(){
+    localStorage.setItem('movieList', JSON.stringify(movieList))
+}
+
+for(const movieInfo of movieList){
+    updateUI(movieInfo);
 }
